c72d00adda009b900190bb97614da075feb84a65
aurotb <auro.tj@gmail.com>
2020-12-24 19:48:29 -0600

    Added json conversion, messages and reorganized algorithm

--- catkin_ws/src/map_visualizer/scripts/map.py
+++ catkin_ws/src/map_visualizer/scripts/map.py
@@ -6,20 +6,53 @@ import json
 class Room:
     def __init__(self, name):
         self.area = []
+        self.obj_int = {}
+        self.path = []
+        self.name = name
+        self.entrance = []
+
+    def __init__(self, name, area):
+        self.name = name
+        self.area = []
+        self.obj_int = {}
+        self.path = []
+        self.entrance = []
+        self.set_area(area)
+
+    def set_area(self, points):
+        for point in points:
+            self.area.append([point.x, point.y, point.z])
+
+    def set_obj_int(self, obj, points):
+        if obj in self.obj_int:
+            obj_int[obj].append(points)
+        else:
+            obj_int[obj] = []
+            obj_int[obj].append(points)
+
+    def set_entrance(self, point):
+        self.entrance = point
+
+    def reset_room(self):
+        self.area = []
         self.obj_int = []
         self.path = []
         self.name = name
-
-    def set_area(self, point):
-        self.area.append(point)
+        self.entrance = None
 
 class Map:
     def __init__(self, name):
         self.rooms = {}
         self.name = name
 
-    def create_room(self, room_name):
-        self.rooms[room_name] = Room(room_name)
+    def __init__(self, name, rooms):
+        self.rooms = {}
+        self.name = name
+        self.set_rooms(rooms)
+
+    def set_rooms(self, room_list):
+        for room in room_list:
+            self.rooms[room.name]=Room(room.name, room.area)
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
--- catkin_ws/src/map_visualizer/scripts/map_tagger.py
+++ catkin_ws/src/map_visualizer/scripts/map_tagger.py
@@ -2,77 +2,96 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 import json
-from map import Map
 from geometry_msgs.msg import Point
 from std_msgs.msg import Int16
+from map_visualizer.msg import MapDisplayState
+from map_visualizer.msg import MapContext
+from map_visualizer.msg import Room
+from map_visualizer.msg import ObjInt
+from map import Map
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "Pose given %s", data.pose.position)
-    global room_name
     global map
-    global point_publisher
-    if(state == 0):
-        map.rooms[room_name].set_area(data.pose.position)
-        point_publisher.publish(data.pose.position)
+    global map_publisher
+    global obj_name
+    if(state.state not in [0,-1]):
+        if state.state == 1:
+            print(len(map.rooms))
+            for index in range(len(map.rooms)):
+                if map.rooms[index].name == state.room:
+                    map.rooms[index].area.append(data.pose.position)
+        elif state.state == 2:
+            for index, obj in map.rooms[state.room].obj_int:
+                if obj.name == obj_name:
+                    map.rooms[state.room].obj_int[index].obj_area.append(data.pose.position)
+        map_publisher.publish(map)
     
-state = -1
-room_name = None
-map = None
-point_publisher = None
+state = MapDisplayState()
+map = MapContext()
+map_publisher = None
 state_publisher = None
+obj_name = None
 
 if __name__ == '__main__':
-    global point_publisher
+    global map_publisher
     global state_publisher
     rospy.init_node('map_tagger')
-    topic = 'map_tagger_pont'
-    point_publisher = rospy.Publisher("map_tagger_pont", Point)
-    state_publisher = rospy.Publisher("map_tagger_state", Int16)
+    topic = 'tagged_map'
+    map_publisher = rospy.Publisher("tagged_map", MapContext)
+    state_publisher = rospy.Publisher("map_tagger_state", MapDisplayState)
     rospy.Subscriber("/move_base_simple/goal", PoseStamped, callback)
 
     rospy.loginfo("Map tagger v1")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("What do you want to do?")
-    print("1. Create map")
-    print("2. Edit map")
-    x = input()
-
     global map
     global state
+    x = 1
     while not rospy.is_shutdown():
         if x == 1:
             x = raw_input("Give me the name of the map: ")
-            map = Map(x)
+            map.name = x
             while True:
                 print("What do you want to do?")
                 print("1. Add a room")
+                print("2. Edit a room")
                 print("0. Exit")
                 x = input()
                 if x == 0:
                     break
                 if x == 1:
-                    state = 0
+                    # x = raw_input('Give me the name of the room: ')
+                    room_name = str(raw_input('Give me the name of the room: '))
+                    state.state = 1
+                    state.room = room_name
                     state_publisher.publish(state)
-                    x = raw_input('Give me the name of the room: ')
-                    room_name = str(x)
-                    map.create_room(room_name)
+                    new_room = Room()
+                    new_room.name = room_name
+                    map.rooms.append(new_room)
+                    # map.create_room(room_name)
                     print("Please choose the enclosing perimeter area of the room with the goal publisher")
                     print("Insert any key to stop")
                     x = raw_input()
                     print("Area saved.")
-                    state = -1
+                    x = input("Save object of interest? (0 = no, 1 = Yes): ")
+                    while(x):
+                        global obj_name
+                        obj_name = str(raw_input('Give me the name of the object: '))
+                        state.state = 2
+                        state_publisher.publish(state)
+                        obj = ObjInt()
+                        obj.name = obj_name
+                        map.rooms[state.room_name].obj_int.append(obj)
+                        print("Please choose the enclosing perimeter area of the object")
+                        print("Insert any key to stop")
+                        x = raw_input()
+                        x = input("Save another object of interest? (0 = no, 1 = Yes): ")
+                    state.state = 0
                     state_publisher.publish(state)
-                    # x = input("Save object of interest? Y/n: ")
-                    # while(str(x) == 'Y'):
-                    #     state = 1
-                    #     state_publisher.publish(state)
-                    #     print("Please choose the enclosing perimeter area of the object")
-                    #     print("Insert any key to stop")
-                    #     x = raw_input()
-                    #     x = raw_input("Save another object of interest? Y/n: ")
         break
-
-    mapJsonStr = map.toJSON()
-    print(mapJsonStr)
+    state.state = -1
+    state_publisher.publish(state)
+    map_obj = Map(map.name, map.rooms)
+    print(map_obj.toJSON())
     with open(str(map.name) + '.json', "w") as f:
-        f.write(mapJsonStr)
+        f.write(map_obj.toJSON())
